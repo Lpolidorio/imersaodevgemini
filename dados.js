f0887de2a80aec402f7458b14a10f74f7f3133c6
Lpolidorio <Lucas.polidorio@gmail.com>
2024-09-07 00:34:21 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -2,26 +2,61 @@ let consoles = [
     {
         titulo: "Atari",
         descricao: "O Atari 2600 foi um dos primeiros videogames domésticos a alcançar grande popularidade, marcando o início de uma nova era no entretenimento. Lançado em 1977, ele revolucionou a forma como as pessoas interagiam com jogos eletrônicos, saindo dos arcades e entrando nas casas.",
-        link: "https://pt.wikipedia.org/wiki/Atari_2600"
+        link: "https://pt.wikipedia.org/wiki.Atari_2600",
+        jogos: [
+            "Top 1 - Berzerk",
+            "Top 2 - Pac-Man",
+            "Top 3 - Mr. Postman",
+            "Top 4 - Frostbite",
+            "Top 5 - Pitfall"
+        ]
     },
     {
         titulo: "Master System",
         descricao: "O Master System foi um console de videogame de 8 bits da Sega, lançado em 1986. Conhecido por seus jogos de alta qualidade gráfica para a época, como Sonic the Hedgehog, o Master System foi um grande sucesso no Brasil.",
-        link: "https://pt.wikipedia.org/wiki/Master_System"
+        link: "https://pt.wikipedia.org/wiki/Master_System",
+        jogos: [
+            "Top 1 - Castle of Illusion Starring Mickey Mouse",
+            "Top 2 - Psycho Fox",
+            "Top 3 - Altered Beast",
+            "Top 4 - The Lucky Dime Caper",
+            "Top 5 - Hang-On"
+        ]
     },
     {
         titulo: "Mega Drive",
         descricao: "O Mega Drive (conhecido como Genesis nos Estados Unidos) foi um console de videogame de 16 bits da Sega, lançado em 1988. Concorrente direto do Super Nintendo, o Mega Drive se destacou por seus jogos de ação e por títulos como Sonic the Hedgehog e Mortal Kombat.",
-        link: "https://pt.wikipedia.org/wiki/Mega_Drive"
+        link: "https://pt.wikipedia.org/wiki/Mega_Drive",
+        jogos: [
+            "Top 1 - Sonic the Hedgehog",
+            "Top 2 - Ultimate Mortal Kombat 3",
+            "Top 3 - Street Fighter 2",
+            "Top 4 - Golden Axe",
+            "Top 5 - Beavis and Butt-Head"
+        ]
     },
     {
         titulo: "PlayStation",
         descricao: "O PlayStation foi o primeiro console de videogame de 32 bits da Sony, lançado em 1994. Com gráficos revolucionários para a época e jogos exclusivos como Final Fantasy VII, o PlayStation marcou uma nova era nos videogames.",
-        link: "https://pt.wikipedia.org/wiki/PlayStation"
+        link: "https://pt.wikipedia.org/wiki/PlayStation",
+        jogos: [
+            "Top 1 - Final Fantasy 9",
+            "Top 2 - Chrono Cross",
+            "Top 3 - Metal Gear Solid",
+            "Top 4 - Tekken 3",
+            "Top 5 - Twisted Metal 3"
+        ]
     },
     {
         titulo: "PC",
         descricao: "O computador pessoal (PC) é uma plataforma de jogos extremamente versátil, capaz de rodar uma infinidade de jogos, desde títulos independentes até grandes blockbusters. A versatilidade e a constante evolução do hardware tornam o PC uma das plataformas de jogos mais populares.",
-        link: "https://pt.wikipedia.org/wiki/Computador_pessoal"
+        link: "https://pt.wikipedia.org/wiki/Computador_pessoal",
+        jogos: [
+            "Top 1 - The Elder Scrolls V: Skyrim",
+            "Top 2 - DOTA 2",
+            "Top 3 - Diablo",
+            "Top 4 - Worms Armageddon",
+            "Top 5 - Age of Empires 2"
+        ]
     }
 ];
